97ea66180c94d2285b3763f217ff073ca03abae8
elina-chrk <elina.cherkasova@nure.ua>
2025-06-12 12:38:32 +0300

    nginx created

--- travelmate-web/src/App.jsx
+++ travelmate-web/src/App.jsx
@@ -70,6 +70,7 @@ function App() {
             }
           />
           <Route path="/activate" element={<ActivateAccountPage />} />
+          
           <Route
             path="/admin"
             element={
